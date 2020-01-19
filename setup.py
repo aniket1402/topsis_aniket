ee0c103214137ed68b5e1d61985d912a2ecc6662
Aniket Gupta <aniketgupta1495@gmail.com>
2020-01-20 01:48:49 +0530

    Second Commit

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   description = 'A muliti-criteria decision analysis method.',
   author = 'Aniket Gupta',
   author_email = 'aniketgupta1495@gmail.com',
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  url = 'https://github.com/aniket1402/topsis_aniket.git',
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],
   install_requires=[
